c5137c0e5e5dfa137a6d1b98199ca7887e93fec7
Guillaume Berche <guillaume.berche@orange.com>
2018-09-24 19:35:25 +0200

    Initial demo steps

---
+++ provider_cloudfoundry.tf
@@ -0,0 +1,38 @@
+provider "cloudfoundry" {
+  api_url             = "https://api.run.pivotal.io"
+  user                = "${var.tf_cf_login}"
+  password            = "${var.tf_cf_password}"
+  skip_ssl_validation = true
+}
+
+data "cloudfoundry_org" "my_org" {
+  name = "bercheg-org"
+}
+
+resource "cloudfoundry_space" "my_space" {
+  name = "demo-basel"
+  org = "${data.cloudfoundry_org.my_org.id}"
+  developers = [
+    //PWS does not allow to lookup users by email, even ourselves
+    //"${data.cloudfoundry_user.myself.id}",
+    "526cc2f7-3d07-4e04-85b2-e3c96282541c"
+     //workaround is to fetch guid from CF CLI:
+     //CF_TRACE=true cf org-users bercheg-org | grep -B10 '"username": "bercheg@gmail.com"' | grep '"guid"'
+  ]
+}
+
+//data "cloudfoundry_user" "myself" {
+//  name = "bercheg@gmail.com"
+//}
+
+
+data "cloudfoundry_service" "elephantsql" {
+  name = "elephantsql"
+}
+
+resource "cloudfoundry_service_instance" "my_mysql" {
+  name = "my_mysql"
+  service_plan = "${data.cloudfoundry_service.elephantsql.service_plans["turtle"]}"
+  space = "${cloudfoundry_space.my_space.id}"
+}
+
