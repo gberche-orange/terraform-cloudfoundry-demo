a4182c7dc2945e62a34e11e6bcc694ee762acaf9
Guillaume Berche <guillaume.berche@orange.com>
2018-10-05 14:22:49 +0200

    Instanciate service

--- provider_cloudfoundry.tf
+++ provider_cloudfoundry.tf
@@ -5,32 +5,35 @@ provider "cloudfoundry" {
   skip_ssl_validation = true
 }
 
+//variable "tf_cf_password" {
+//  default="redacted"
+//}
+//variable "tf_cf_login" {
+//  default="bercheg@redacted.com"
+//}
+
+
 data "cloudfoundry_org" "my_org" {
   name = "bercheg-org"
-}
-
-resource "cloudfoundry_user" "demo-user" {
-  name = "demo@mydomain.com"
-  password = "this-account-wont-last-long"
 }
 
 resource "cloudfoundry_space" "my_space" {
   name = "demo-basel"
   org = "${data.cloudfoundry_org.my_org.id}"
   developers = [
-    "${cloudfoundry_user.demo-user.id}",
     //PWS does not allow to lookup users by email, even ourselves
     //"${data.cloudfoundry_user.myself.id}",
+
+    //Refering to an existing user by its guid
     "526cc2f7-3d07-4e04-85b2-e3c96282541c"
-     //workaround is to fetch guid from CF CLI:
-     //CF_TRACE=true cf org-users bercheg-org | grep -B10 '"username": "bercheg@gmail.com"' | grep '"guid"'
+    //workaround is to fetch guid from CF CLI:
+    //CF_TRACE=true cf org-users bercheg-org | grep -B10 '"username": "bercheg"' | grep '"guid"'
+
+    // PWS does not allow for users creation using CC API, requires webui UX
+    //    "${cloudfoundry_user.demo-user.id}",
+
   ]
 }
-
-//data "cloudfoundry_user" "myself" {
-//  name = "bercheg@gmail.com"
-//}
-
 
 data "cloudfoundry_service" "elephantsql" {
   name = "elephantsql"
@@ -42,3 +45,13 @@ resource "cloudfoundry_service_instance" "my_mysql" {
   space = "${cloudfoundry_space.my_space.id}"
 }
 
+
+
+
+
+
+// PWS does not allow for users creation using CC API, requires webui flow for this
+//resource "cloudfoundry_user" "demo-user" {
+//  name = "demo@mydomain.com"
+//  password = "this-account-wont-last-long"
+//}
